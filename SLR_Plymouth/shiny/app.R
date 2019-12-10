7603c3f64f2df7346545348bb932538a60873933
zibbini <ziadibbini@gmail.com>
2019-12-10 13:19:45 +0000

    Add links and text

--- SLR_Plymouth/shiny/app.R
+++ SLR_Plymouth/shiny/app.R
@@ -5,6 +5,10 @@ library(raster)
 
 plymCRS <- "+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.999601 +x_0=400000 +y_0=-100000 +ellps=airy +towgs84=446.448, 
            -125.157,542.060,0.1502,0.2470,0.8421,-20.4894 +units=m +no_defs"
+url_met <- a("here.", 
+             href = "https://www.metoffice.gov.uk/binaries/content/assets/metofficegovuk/pdf/research/ukcp/ukcp18-guidance---representative-concentration-pathways.pdf", 
+             target = "_blank")
+url_git <- a("GitHub page", href = "https://github.com/zibbini/minor_projects/tree/master/SLR_Plymouth/shiny", target = "_blank")
 
 ui <- fluidPage(
     titlePanel("The impact of sea level rise on Plymouth"),
@@ -20,6 +24,14 @@ ui <- fluidPage(
                     value = 2019, 
                     step = 1, 
                     sep = ""),
+        tags$div(header = TRUE, checked = NA,
+                 tags$b("Information:"),
+                 br(),
+                 tagList("Guidance on how to intepret the various representative concentration pathways (RCP) can be found ", url_met),
+                 br(),
+                 br(),
+                 tagList("For more information on this dashboard, please refer to the following ", url_git, icon("github"))
+        )
     ),
     mainPanel(
         leafletOutput(outputId = "map")
